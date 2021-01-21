58cd9a2e1bd4a83f2c2ba0a9b81160e7a6ee6719
Yuji Miyakawa <yuji.mod105@gmail.com>
2021-01-21 23:04:10 +0900

    My Django Girls app, first commit

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def post_list(request):
+    contents = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/post_list.html', contents)
+    
